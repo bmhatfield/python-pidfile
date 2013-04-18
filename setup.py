548ae5247a5d0984c5549853d229ff2fc6c5f92e
Brian Hatfield <bhatfield@brightcove.com>
2013-04-18 12:36:09 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import sys
+import os
+from distutils.core import setup
+
+setup(name='pidfile',
+      version=0.1.0,
+      description='A Pidfile Context Manager compatible with python-daemon\'s DaemonContext()',
+      author="Brian Hatfield",
+      author_email='bmhatfield@gmail.com',
+      url='https://github.com/bmhatfield/python-pidfile.git',
+      py_modules=['pidfile'],
+      license='MIT'
+     )
+
+
+
